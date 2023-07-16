1ad6952303804607d977ccd916cba9e8e9d913f3
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-07-16 18:21:46 -0300

    card project struture

--- portifolio_v.1.0/src/components/ProjectsContainer.jsx
+++ portifolio_v.1.0/src/components/ProjectsContainer.jsx
@@ -1,15 +1,24 @@
 import '../styles/components/projectscontainer.sass'
 
+import FrontWineShop from '../img/frontWineshop.png'
+
 const ProjectsContainer = () => {
   return (
     <section id="projects-container">
       <h2>Projetos</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi animi quos odit rem impedit. Ipsam nobis repudiandae, a unde quaerat hic deserunt cumque quibusdam laborum quis veniam distinctio repellendus quasi.
-      </p>
-      <a href="" className="btn">
-        Ver Projetos
-      </a>
+      <div className='project-card'>
+        <div className="card-image">
+          <img src={FrontWineShop} alt="" />
+        </div>
+        <div className="card-info">
+          <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi animi quos odit rem impedit. Ipsam nobis repudiandae, a unde quaerat hic deserunt cumque quibusdam laborum quis veniam distinctio repellendus quasi.
+          </p>
+          <a href="" className="btn">
+          Ver Projetos
+          </a>
+        </div>
+      </div>
     </section>
   )
 }
